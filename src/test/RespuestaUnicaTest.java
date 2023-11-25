e5cb11d7bf292c7f7cdedd1de1158a255894a8de
Carlos <131988778+JCLV2002@users.noreply.github.com>
2023-11-24 19:22:02 -0700

    Ajuste
    Ajuste el formato y borre unos enter

--- src/test/RespuestaUnicaTest.java
+++ src/test/RespuestaUnicaTest.java
@@ -2,14 +2,9 @@ package test;
 
 import java.util.Arrays;
 import java.util.List;
-
-
-
 import Strategies.PreguntaRespuestaUnica;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
-
 
 public class RespuestaUnicaTest {
 
@@ -17,11 +12,8 @@ public class RespuestaUnicaTest {
     public void testGetSelectedRespuesta() {
         List<String> opciones = Arrays.asList("Opción A", "Opción B", "Opción C");
         PreguntaRespuestaUnica pregunta = new PreguntaRespuestaUnica("¿Cuál es tu opción favorita?", opciones);
-
         assertNull(pregunta.getSelectedRespuesta());
-
         pregunta.setSelectedRespuesta(2);
-
         assertEquals(2, pregunta.getSelectedRespuesta());
     }
 
@@ -29,11 +21,8 @@ public class RespuestaUnicaTest {
     public void testSetSelectedRespuesta() {
         List<String> opciones = Arrays.asList("Opción A", "Opción B", "Opción C");
         PreguntaRespuestaUnica pregunta = new PreguntaRespuestaUnica("¿Cuál es tu opción favorita?", opciones);
-
-
         assertThrows(IllegalArgumentException.class, () -> pregunta.setSelectedRespuesta(0));
         assertThrows(IllegalArgumentException.class, () -> pregunta.setSelectedRespuesta(4));
-
         // Seleccionar una opción válida debería funcionar sin problemas
         pregunta.setSelectedRespuesta(2);
         assertEquals(2, pregunta.getSelectedRespuesta());
@@ -43,10 +32,8 @@ public class RespuestaUnicaTest {
     public void testEquals() {
         List<String> opciones1 = Arrays.asList("Opción A", "Opción B", "Opción C");
         List<String> opciones2 = Arrays.asList("Opción A", "Opción B", "Opción C");
-
         PreguntaRespuestaUnica pregunta1 = new PreguntaRespuestaUnica("¿Cuál es tu opción favorita?", opciones1);
         PreguntaRespuestaUnica pregunta2 = new PreguntaRespuestaUnica("¿Cuál es tu opción favorita?", opciones2);
-
         assertEquals(pregunta1, pregunta2);
 
         // Cambiar una opción debería hacer que las preguntas no sean iguales
@@ -59,22 +46,18 @@ public class RespuestaUnicaTest {
     public void testToString() {
         List<String> opciones = Arrays.asList("Opción A", "Opción B", "Opción C");
         PreguntaRespuestaUnica pregunta = new PreguntaRespuestaUnica("¿Cuál es tu opción favorita?", opciones);
-
         String expectedToString = "¿Cuál es tu opción favorita?\n" +
                 "1. Opción A\n" +
                 "2. Opción B\n" +
                 "3. Opción C\n";
-
         assertEquals(expectedToString, pregunta.toString());
     }
     @Test
     public void testEqualsDifferentEnunciado() {
         List<String> opciones1 = Arrays.asList("Opción A", "Opción B", "Opción C");
         List<String> opciones2 = Arrays.asList("Opción A", "Opción B", "Opción C");
-
         PreguntaRespuestaUnica pregunta1 = new PreguntaRespuestaUnica("¿Cuál es tu opción favorita?", opciones1);
         PreguntaRespuestaUnica pregunta2 = new PreguntaRespuestaUnica("¿Cuál es tu opción preferida?", opciones2);
-
         // Dos preguntas con enunciados diferentes no deberían ser iguales
         assertNotEquals(pregunta1, pregunta2);
     }
